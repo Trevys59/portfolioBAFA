b814f790027eb785d09d7fd0f02636bcd49a19a8
Trevys59 <trevys.sarrazyn@gmail.com>
2024-11-22 08:32:54 +0100

    V2.5.4.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ document.querySelectorAll('#nav-links a').forEach(link => {
   });
   
   // Gestion des pop-ups pour les cartes
-  const cards = document.getElementById('cardpop'); //.card
+  const cards = document.querySelectorAll('.card2');
   const popup = document.getElementById('popup');
   const popupTitle = document.getElementById('popup-title');
   const popupText = document.getElementById('popup-text');
